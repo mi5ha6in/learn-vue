1a6d71d0fe86bca5fed70e544b4a964b58ccb317
mi5ha6in <mi5ha6in@gmail.com>
2025-03-07 06:51:03 +0300

    adds vue3 style example

---
+++ reactivity/vue3StyleExample/src/vue-project/src/main.js
@@ -0,0 +1,32 @@
+import { ref, computed, watchEffect } from "vue";
+
+const counterButton = document.querySelector("button#counter");
+const resetButton = document.querySelector("button#reset");
+
+const counterState = ref(5);
+const isCounterTooBig = computed(() => counterState.value > 10);
+
+watchEffect(renderCounter);
+watchEffect(updateCounterColor);
+
+function renderCounter() {
+  console.log("html");
+  counterButton.textContent = `counter: ${counterState.value}`;
+}
+
+function updateCounterColor() {
+  console.log("color");
+  counterButton.classList.toggle("red", isCounterTooBig.value);
+}
+
+counterButton.addEventListener("click", () => {
+  counterState.value += 1;
+});
+
+resetButton.addEventListener("click", () => {
+  counterState.value = 0;
+});
+
+setInterval(() => {
+  counterState.value += 1;
+}, 1000);
